3802b9fe628c62f3113aad970fb34fba236dd553
Rami-Louati <rami.louati@edu.dsti.institute>
2023-01-10 15:29:53 +0100

    conflict

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from math import cos
-
+import seaborn as sns
 
 if __name__ == "__main__":
     for i in range(314):
-        print(cos(i / 100))
+        print(cos(i / 10))
+elif: 
+    print("it was a good day")
